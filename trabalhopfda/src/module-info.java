20e720efff6948c2967b0960183638487b65d835
Igor-Franco <igorffranco@gmail.com>
2022-05-22 21:11:43 -0300

    up

---
+++ trabalhopfda/src/module-info.java
@@ -0,0 +1,2 @@
+module trabalhopfda {
+}
---
+++ trabalhopfda/src/trabalhopfda/Trabalhopfda.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package trabalhopfda;
+
+/**
+ * @author igorffranco
+ *
+ */
+public class Trabalhopfda {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
